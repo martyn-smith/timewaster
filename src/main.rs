4992c941018776f4439df9c72e2f6c11b114239b
Martyn <martyn.smith560@outlook.com>
2022-01-04 18:18:25 +0000

    printing bugfix; added tests and reference python

--- src/main.rs
+++ src/main.rs
@@ -14,19 +14,25 @@ struct Opt {
     num_threads: usize,
 }
 
-fn try_hash(tx: mpsc::Sender<Vec<u8>>, solved: Arc<AtomicBool>, difficulty: usize) {
+/*
+ * CONSIDER: not checking for a solution every round.
+ * The downside of this, other than slightly higher complication, is a
+ * slight inefficiency; if we only check every n times, up to n hashes
+ * are "wasted" (per thread). But overall we'd expect a speedup.
+ */
+fn hunt(tx: mpsc::Sender<Vec<u8>>, solved: Arc<AtomicBool>, difficulty: usize) {
     let mut rng = rand::thread_rng();
-    let mut x = vec![0u8; 64];
+    let mut cand = vec![0u8; 64];
 
     loop {
         if solved.load(Ordering::SeqCst) {
             break;
         }
-        rng.fill(&mut x[..]);
-        let cand = Sha256::digest(&x);
-        if cand[0..difficulty] == x[0..difficulty] {
+        rng.fill(&mut cand[..]);
+        let test = Sha256::digest(&cand);
+        if test[0..difficulty] == cand[0..difficulty] {
             solved.store(true, Ordering::SeqCst);
-            tx.send(x).unwrap();
+            tx.send(cand).unwrap();
             break;
         }
     }
@@ -42,13 +48,42 @@ fn main() {
             let tx = tx.clone();
             let solved = solved.clone();
             let difficulty = opt.difficulty;
-            thread::spawn(move || try_hash(tx, solved, difficulty))
+            thread::spawn(move || hunt(tx, solved, difficulty))
         })
         .collect::<Vec<thread::JoinHandle<_>>>();
     for h in handles {
         h.join().unwrap();
     }
     let answer = rx.recv().unwrap();
-    let msg = answer.iter().fold(String::new(), |out, i| format!("{}{:01x}", out, i));
+    let msg = answer.iter().fold(String::new(), |out, i| format!("{}{:02x}", out, i));
     println!("{}\tpartially matches its own hash.", msg);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn known_hash() {
+        let matching = "d7d0ffe6d449ecd1cb391f7e1c5b348c8762c2f48b59706f11e0fcef40dd6a92248937334a96e81d59db10eea775bd1630f2bacd403f83f2b44cf309876176b2";
+        let m = matching.as_bytes()
+                        .chunks(2)
+                        .map(|c| u8::from_str_radix(std::str::from_utf8(c).unwrap(), 16).unwrap())
+                        .collect::<Vec<u8>>();
+        let test = Sha256::digest(&m);
+        assert_eq!(test[0..3], m[0..3]);
+        let msg = m.iter().fold(String::new(), |out, i| format!("{}{:02x}", out, i));
+        assert_eq!(matching, msg);
+    }
+
+    #[test]
+    fn unknown_hash() {
+        let solved = Arc::new(AtomicBool::new(false));
+        let (tx, rx) = mpsc::channel::<Vec<u8>>();
+        hunt(tx, solved, 3);
+        let m = rx.recv().unwrap();
+        let test = Sha256::digest(&m);
+        assert_eq!(test[0..3], m[0..3]);
+    }
+
 }
